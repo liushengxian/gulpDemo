d0eccfeb9bd6f081fa222ce1b5a88122e2ffc9ba
圣籼 刘 <shengxian.liu@palmaplus.com>
2017-11-07 21:27:50 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require("gulp");
+var less = require("gulp-less");
+var watch = require("gulp-watch");
+var runSequence = require("run-sequence");
+var rm = require("del");
+var rename = require("gulp-rename");
+var minifycss = require("gulp-clean-css");
+var uglify = require("gulp-uglify");
+var connect = require("gulp-connect");
+
+var htmlPath = 'src/html/*.html';
+var lessPath = 'src/less/*.less';
+var jsPath = 'src/js/*.js';
+var imgPath = 'src/assets/**.*';
+
+
+gulp.task("clean", function() {
+  return rm("dist", function() {
+    console.log("gulp clean ok!");
+  });
+});
+
+gulp.task("html", function() {
+  return gulp.src(htmlPath).pipe(gulp.dest("dist")).pipe(connect.reload());
+});
+
+gulp.task("less", function() {
+  return (gulp
+      .src(lessPath)
+      // .pipe(watch('src/less/*.less'))
+      .pipe(less())
+      .pipe(minifycss({ compatibility: "ie8" }))
+      .pipe(
+        rename(function(path) {
+          path.basename += ".min";
+        })
+      )
+      .pipe(gulp.dest("dist/css")).pipe(connect.reload()));
+});
+
+gulp.task("img", function() {
+  return gulp.src(imgPath).pipe(gulp.dest("dist/assets")).pipe(connect.reload());
+});
+
+gulp.task("js", function() {
+  return gulp
+    .src(jsPath)
+    .pipe(uglify())
+    .pipe(rename(function(path){
+        path.basename += ".min";
+    }))
+    .pipe(gulp.dest("dist/js")).pipe(connect.reload());
+});
+
+gulp.task("connect",function(){
+    connect.server({
+        root:'dist',
+        livereload: true
+    });
+})
+
+gulp.task("livereload",function(){
+    gulp.src("dist/**.*").pipe(watch()).pipe(connect.reload());
+})
+
+gulp.task("watch",function(){
+    gulp.watch(lessPath,['less']);
+    gulp.watch(jsPath,['js']);
+    gulp.watch(htmlPath,['html']);
+    gulp.watch(imgPath,['img']);
+})
+
+gulp.task("beatit", function() {
+  runSequence("clean", ["html", "less", "img", "js"],["connect","watch"], function() {
+    console.log("BEATIT~ BEATIT~ 2");
+  });
+  console.log("BEATIT~ BEATIT~ 1");
+});
